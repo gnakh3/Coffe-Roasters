d8af48d02ded5a9307cc558e320d7770fdfc36a4
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2025-02-03 23:24:43 +0400

    created page 3,4,footer

---
+++ script.js
@@ -0,0 +1,5 @@
+const button2 = document.getElementById('create-bttn2')
+button2.addEventListener('click',function (event) {
+    event.preventDefault();
+    window.location.href = "create.html";
+})
