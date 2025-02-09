a2a39f13453d9dddb0c151d57b720389db874c58
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2025-02-09 21:26:56 +0400

    Fix minor bugs

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ document.getElementById("aboutus").addEventListener("click", function() {
 document.getElementById("createyourplan").addEventListener("click", function() {
     window.location.href = "subscribe.html";
 });
+
+
+document.querySelectorAll(".headerpic").forEach(element => {
+    element.addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+});
