e4408f93f7c80ddd3c1d3c1f7e3cb5b8c7141858
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2025-02-09 21:14:04 +0400

    Revert "Fixed minor bugs and  CPR 26"

--- script.js
+++ script.js
@@ -9,10 +9,3 @@ document.getElementById("aboutus").addEventListener("click", function() {
 document.getElementById("createyourplan").addEventListener("click", function() {
     window.location.href = "subscribe.html";
 });
-
-
-document.querySelectorAll(".headerpic").forEach(element => {
-    element.addEventListener("click", function() {
-        window.location.href = "index.html";
-    });
-});
