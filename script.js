681a77afc801456ab56cee259cc19a3ba1877f91
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2025-02-13 19:06:12 +0400

    fix bugs

--- script.js
+++ script.js
@@ -27,3 +27,30 @@ document.getElementById("createplanbutton1").addEventListener("click", function(
     e.preventDefault();
      window.location.href = "subscribe.html";
  })
+ document.getElementById("facebook-icon").addEventListener('mouseenter',function(e) {
+    e.preventDefault();
+    document.getElementById("Path1").style.fill = "#FDD6BA";
+    document.getElementById("Path1").style.transition = "fill 0.3s ease;";
+ })
+ document.getElementById("facebook-icon").addEventListener('mouseleave',function(e) {
+    e.preventDefault();
+    document.getElementById("Path1").style.fill = "#FEFCF7";
+ })
+ document.getElementById("twiter-icon").addEventListener('mouseenter',function(e) {
+    e.preventDefault();
+    document.getElementById("Path2").style.fill = "#FDD6BA";
+    document.getElementById("Path2").style.transition = "fill 0.3s ease;";
+ })
+ document.getElementById("twiter-icon").addEventListener('mouseleave',function(e) {
+    e.preventDefault();
+    document.getElementById("Path2").style.fill = "#FEFCF7";
+ })
+ document.getElementById("instagram-icon").addEventListener('mouseenter',function(e) {
+    e.preventDefault();
+    document.getElementById("Shape").style.fill = "#FDD6BA";
+    document.getElementById("Shape").style.transition = "fill 0.3s ease;";
+ })
+ document.getElementById("instagram-icon").addEventListener('mouseleave',function(e) {
+    e.preventDefault();
+    document.getElementById("Shape").style.fill = "#FEFCF7";
+ })
